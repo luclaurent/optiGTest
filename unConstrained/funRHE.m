6957bb0b1213b8b93d2e16aae0124b0594689528
Luc <l2@crans.org>
2018-04-17 17:25:46 +0200

    Rewrite many functions

---
+++ unConstrained/funRHE.m
@@ -0,0 +1,38 @@
+%% Rotated hyper-ellipsoid function
+%L. LAURENT -- 21/02/2012 -- luc.laurent@lecnam.net
+
+% optiGTest - set of testing functions    A toolbox to easy manipulate functions.
+% Copyright (C) 2017  Luc LAURENT <luc.laurent@lecnam.net>
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%
+%1 global minimum : x=(0,0,...,0) >> f(x)=0
+%
+%design space: -65.536<xi<65.536
+
+function [p,dp]=funRHE(xx)
+
+%number of designa variables
+nbvar=size(xx,3);
+
+%responses and derivatives
+vv=reshape(nbvar:-1:1,1,1,nbvar);
+coef=repmat(vv,[size(xx,1) size(xx,2)]);
+cal=coef.*xx.^2;
+p=sum(cal,3);
+if nargout==2
+    dp=2*coef.*xx;
+end
+end
