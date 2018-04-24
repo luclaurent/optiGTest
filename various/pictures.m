8fa1e4f41263f616df48ec80e445cddd56f4093c
Luc LAURENT <luc.laurent@lecnam.net>
2018-04-24 17:48:57 +0200

    add png with lfs

--- various/pictures.m
+++ various/pictures.m
@@ -1,9 +1,12 @@
 %%%% Create pitcures for all cases
+clear all
+close all
 
 %% unconstrained
-
-
 addpath('..')
+
+%file for wiki
+ff=fopen('unConstrainedWiki.txt','w');
 
 %load class
 pb=unConstrained('');
@@ -12,7 +15,7 @@ listFun=loadDim();
 % names of functions
 nameFun=fieldnames(listFun);
 % along functions
-for itF=1:numel(nameFun{1:4})
+for itF=1:4%numel(nameFun{1:4})
     disp(itF)
     %dimension 2 only
     if ismember(listFun.(nameFun{itF}),2)||any(isinf(listFun.(nameFun{itF})))
@@ -25,9 +28,14 @@ for itF=1:numel(nameFun{1:4})
         filePDF=[file,'.pdf'];
         filePNG=[file,'.png'];
         %saveas(h,fileEPS,'-epsc')
-        print(h,filePDF,'-dpdf','-painters')
+        %print(h,filePDF,'-dpdf','-painters')
         saveas(h,filePNG)
         %close figure
         pb.closeFig;
+        %write in the wiki's file
+        fprintf(ff,'#%s\n',nameFun{itF});
+        fprintf(ff,'![%s](%s)\n',nameFun{itF},...
+            ['https://bytebucket.org/luclaurent/optigtest/various/',filePNG]);
     end
 end
+fclose(ff);
