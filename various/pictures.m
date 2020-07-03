8e8c3fda3a2cc4e530fa91ae39714899b8821ee0
L2 <luclaurent@crans.org>
2020-07-03 16:23:51 +0200

    remove all pictures and modify procedure to populate wiki

--- various/pictures.m
+++ various/pictures.m
@@ -1,42 +1,53 @@
-%%%% Create pitcures for all cases
+%%%% Create pcture for all cases
 clear all
 close all
 
-%% unconstrained
+%% add root to path
 addpath('..')
-
-%file for wiki
-ff=fopen('Wiki.txt','w');
 
 %load class
 pb=optiGTest('');
-% load list of functions
-listFun=loadDimMulti();
-% names of functions
-nameFun=fieldnames(listFun);
-% along functions
-for itF=1:numel(nameFun)
-    disp(itF)
-    %dimension 2 only
-    if any(ismember(listFun.(nameFun{itF}),2))||any(isinf(listFun.(nameFun{itF})))
-        %load function
-        pb.namePb=nameFun{itF};
-        %plot demo
-        h=pb.demo;
-        %obtain pdf and png
-        file=fullfile('various','Figures','MultiObj',nameFun{itF});
-        fileEPS=[file,'.eps'];
-        filePDF=[file,'.pdf'];
-        filePNG=[file,'.png'];
-        saveas(h(end),fileEPS,'epsc')
-        print(h(end),filePDF,'-dpdf','-painters')
-        saveas(h(end),filePNG)
-        %close figure
-        pb.closeFig;
-        %write in the wiki's file
-        fprintf(ff,'#%s\n',nameFun{itF});
-        fprintf(ff,'![%s](%s)\n',nameFun{itF},...
-            ['https://bitbucket.org/luclaurent/optigtest/raw/master/various/',filePNG]);
+
+listCase={'unConstrained','Constrained','MultiObj'};
+listLoadFile={'loadDimUn','loadDimCons','loadDimMulti'};
+
+for itC = 1:numel(listCase)
+    %file for wiki
+    ff=fopen([listCase{itC} '.txt'],'w');
+    % load list of functions
+    listFun=feval(listLoadFile{itC});
+    % names of functions
+    nameFun=fieldnames(listFun);
+    % along functions
+    for itF=1:numel(nameFun)
+        
+        %dimension 2 only
+        if any(ismember(listFun.(nameFun{itF}),2))||any(isinf(listFun.(nameFun{itF})))
+            %load function
+            pb.namePb=nameFun{itF};
+            disp(itF)
+            disp(nameFun{itF});
+            %plot demo
+            h=pb.demo;
+            %create dir
+            if ~exist(fullfile('wiki','Figures',listCase{itC}),'dir')
+                mkdir(fullfile('wiki','Figures',listCase{itC}));
+            end
+            %obtain pdf and png
+            file=fullfile('wiki','Figures',listCase{itC},nameFun{itF});
+            %fileEPS=[file,'.eps'];
+            %filePDF=[file,'.pdf'];
+            filePNG=[file,'.png'];
+            %saveas(h(end),fileEPS,'epsc')
+            %print(h(end),filePDF,'-dpdf','-painters')
+            saveas(h(end),filePNG)
+            %close figure
+            pb.closeFig;
+            %write in the wiki's file
+            fprintf(ff,'#%s\n',nameFun{itF});
+            fprintf(ff,'![%s](%s)\n',nameFun{itF},...
+                ['Figures/',listCase{itC},'/',filePNG]);
+        end
     end
+    fclose(ff);
 end
-fclose(ff);
